47d58f842a65249920d6ca158dab90fa8b7828e0
Oltion-Koceku <koceku17@gmail.com>
2024-03-19 15:56:45 +0100

    [bonus]

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ createApp({
       apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
       counter: 0,
       email: [],
-
+      emailDaStampare: []
     };
   },
 
@@ -18,10 +18,11 @@ createApp({
             this.email.push(res.data.response);
             this.counter++;
             this.getEmail();
-            if (this.email.length === 10) {
+            if (this.email.length >= 10) {
               console.log("siamo a 10 email");
+              this.emailDaStampare.push(this.email)
             }
-            console.log(this.email);
+            console.log(this.emailDaStampare);
           })
           .catch(error => {
             console.log(error.message);
